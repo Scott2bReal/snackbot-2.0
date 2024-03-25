e8f223e4a3eb3f45502b71e9f2ec0ff27ece8b20
Scott Hoecker <hoeckers@gmail.com>
2024-03-25 16:57:52 -0500

    Add DB connection string to config

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,17 @@
 const token = process.env.DISCORD_BOT_TOKEN
 const clientId = process.env.DISCORD_CLIENT_ID
+const databaseUrl = process.env.DATABASE_URL
 
 if (!token || !clientId) {
   throw new Error("DISCORD_BOT_TOKEN and DISCORD_CLIENT_ID must be provided")
 }
 
+if (!databaseUrl) {
+  throw new Error("DATABASE_URL must be provided")
+}
+
 export const config = {
   token,
   clientId,
+  databaseUrl,
 }
