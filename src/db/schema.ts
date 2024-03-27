c772e9ae83adcf503442345f5f8de7e3fb2c82d5
Scott Hoecker <hoeckers@gmail.com>
2024-03-26 20:23:10 -0500

    Delete unused code

--- src/db/schema.ts
+++
@@ -1,9 +0,0 @@
-import { drizzle } from "drizzle-orm/node-postgres"
-import { Pool } from "pg"
-import { config } from "../config"
-
-const pool = new Pool({
-  connectionString: config.databaseUrl,
-})
-
-export const db = drizzle(pool)
